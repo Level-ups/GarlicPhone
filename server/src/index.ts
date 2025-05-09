67664a8f0b4ffb001be1b523ae192886114bb3da
Katlego Sekoele <katlego.sekoele@bbd.co.za>
2025-05-09 20:37:11 +0200

    testing if redeploy works

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ app.get('/events/health', createServerSentEventHandler<string>(sendEvent => {
 
 // Start server
 app.listen(Number(process.env.PORT) || 5000, '0.0.0.0', () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
 export default app;
